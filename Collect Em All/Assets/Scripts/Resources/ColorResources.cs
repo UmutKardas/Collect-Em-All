01d1e7e6eea2d23ba218e6c6a382a3d8b8d39841
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-03-20 23:21:14 +0300

    Create color scriptable data and edited

---
+++ Collect Em All/Assets/Scripts/Resources/ColorResources.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Resources
+{
+    [CreateAssetMenu(fileName = "ColorResources", menuName = "Scriptable/ColorResources")]
+    public class ColorResources : ScriptableObject
+    {
+        [SerializeField] private List<ColorData> colorDatas;
+
+        public ColorData GetColor(ColorType colorType)
+        {
+            return colorDatas.Find(x => x.ColorType == colorType);
+        }
+
+        public ColorData GetRandomColor()
+        {
+            return colorDatas[Random.Range(0, colorDatas.Count)];
+        }
+    }
+
+    public enum ColorType
+    {
+        None,
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Purple,
+        Orange
+    }
+
+    [System.Serializable]
+    public struct ColorData
+    {
+        public ColorType ColorType;
+        public Color Color;
+    }
+}
